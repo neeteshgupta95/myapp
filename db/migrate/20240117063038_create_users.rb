fb31694e6aafd6cc91ac49c05c424ab0eec28e08
Neetesh Gupta <neetesh.gupta95@gmail.com>
2024-01-17 13:52:50 +0530

    pry and activerecord-import gem added

--- db/migrate/20240117063038_create_users.rb
+++ db/migrate/20240117063038_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :uuid, limit: 36
+      t.string :uuid, limit: 36, unique: true
       t.string :gender, limit: 6
       t.jsonb :name
       t.jsonb :location
