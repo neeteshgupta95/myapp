ee40d31facb81c22f3a0a56873494ecae1bac12e
Neetesh Gupta <neetesh.gupta95@gmail.com>
2024-01-17 12:04:53 +0530

    created user model and its table

---
+++ db/migrate/20240117063038_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :uuid, limit: 36
+      t.string :gender, limit: 6
+      t.jsonb :name
+      t.jsonb :location
+      t.integer :age
+
+      t.timestamps
+    end
+  end
+end
